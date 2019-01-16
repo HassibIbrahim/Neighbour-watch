f121607f5978393feec38749345ce92799e5ee3c
HassibIbrahim <ihassib@gmail.com>
2019-01-16 15:46:55 +0300

    migations of newly added models

--- mtaa/forms.py
+++ mtaa/forms.py
@@ -2,12 +2,12 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from .models import *
 
- class NewProfileForm(forms.ModelForm):
+class NewProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
         exclude = ['user', 'business']
 
- class BusinessForm(forms.ModelForm):
+class BusinessForm(forms.ModelForm):
     class Meta:
         model = Business
         exclude = ['user']
